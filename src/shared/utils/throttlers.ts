8d8f4a4bf3768fd158ec70001e0d2de7ee779737
Faye0612 <uzamukundaperuoth@gmail.com>
2026-07-09 15:41:13 +0800

    合并远程删除的文件

--- src/shared/utils/throttlers.ts
+++
@@ -1,6 +0,0 @@
-import { createThrottler, DEFAULT_THROTTLE_MS } from './throttle';
-
-export const thsThrottler = createThrottler(DEFAULT_THROTTLE_MS);
-export const eastmoneyThrottler = createThrottler(DEFAULT_THROTTLE_MS);
-export const cailianpressThrottler = createThrottler(DEFAULT_THROTTLE_MS);
-export const gelonghuiThrottler = createThrottler(DEFAULT_THROTTLE_MS);
